b610b1c7bda789ae70377a90086ea4e4588c3e7e
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-21 19:27:35 +0530

    displayed variables

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hi how are you"
+echo "$PERSON2:: iam good how about you"
+echo "$PERSON1:: i have a deal for you ?"
+echo "$PERSON2:: what that ?"
+echo "$PERSON1:: nothing bye"
