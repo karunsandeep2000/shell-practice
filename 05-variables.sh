354f963145c5853fff34ef7cc784478e36b80bf7
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-21 19:47:31 +0530

    pin number

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Please enter your PIN-NUMBER:"
-read -s  PIN-NUMBER
+read -sPIN-NUMBER
 echo "Hello, $PINNUMBER!"                                                                                                                                                                                                                                                        
