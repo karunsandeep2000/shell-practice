1be2bcca9dd2f2e93f4a4b1646688169ca572289
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-21 10:13:55 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+PERSON1=KUMAR
+PERSON2=KUMARI
 
+echo $PERSON1- "hi $PERSON1 who are you"
+echo $PERSON2- "hi $PERSON1 i am good"
+echo $PERSON1- "iam learining devsecops"
+echo $PERSON1- "that's sounds good please teach me also ?"
