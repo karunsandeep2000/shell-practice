c194fe5be207e1c379d4fede8e29a03f9bcb588d
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-22 18:41:18 +0530

    variables

--- 08-variables.sh
+++ 08-variables.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+DATE=$(date)
 
-echo "$PERSON1:: hi how are you"
-echo "$PERSON2:: iam good how about you"
-echo "$PERSON1:: i have a deal for you ?"
-echo "$PERSON2:: what that ?"
-echo "$PERSON1:: nothing bye"
+$DATE
