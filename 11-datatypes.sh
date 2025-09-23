cc87bdd3e1c4435458c8064de32ba2777cc87e06
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-23 12:24:04 +0530

    datatypes

--- 11-datatypes.sh
+++ 11-datatypes.sh
@@ -2,9 +2,9 @@
 
 Number=$1
 
-if [ $Number -lt 10 ] then;
+if [ $Number -lt 10 ]; then
     echo "Number is less than $Number"
 
-else [ $Number -gt 10 ] then;
+else [ $Number -gt 10 ]; then
     echo "Number is greater than $Number"
 fi
