ca95d5e30d956ee7325bd8ef4e7a64028edc34e2
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-23 13:50:32 +0530

    datatypes

--- 13-datatypes.sh
+++ 13-datatypes.sh
@@ -2,7 +2,7 @@
 
 echo "please entre your number"
 read number
-    if[ $(($number % 1)) ]; then
+    if[ $(($number % 1)) 0 ]; then
 echo "given number is $number is even"
     else 
 echo "given number $number is odd"
