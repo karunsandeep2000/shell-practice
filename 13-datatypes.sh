a6f13ac10d7a2e4b30f3c4c5a429ffab283e2f1c
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-23 14:18:06 +0530

    datatypes

--- 13-datatypes.sh
+++ 13-datatypes.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-echo "please entre your number"
-read number
-    if [ $(($number % 3)) -eq 0 ]; then
-echo "given number is $number is even"
-    else 
-echo "given number $number is odd"
-fi
+echo "hi  ravi "
