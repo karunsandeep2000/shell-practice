f8469a6a649087aa4dde63d15292ab70da37cd0c
karunsandeep2000 <karunsandeep2000@gamil.com>
2025-09-23 16:46:47 +0530

    datatypes

--- 13-datatypes.sh
+++ 13-datatypes.sh
@@ -3,12 +3,14 @@
 user=$(id -u)
     if [ $user -nq 0 ]; then 
 echo "please go with root user"
+exit 1
     fi
 
 dnf install mysql -y 
 
     if [ $? -nq 0 ]; then 
 echo "try again"
+exit 1
     else
 echo "ok installed"
 fi 
