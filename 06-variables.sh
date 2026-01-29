5d9a71f43832c19f7e8220838a05f0de60aa6972
karunsandeep2000 <karunsandeep2000@gamil.com>
2026-01-29 17:22:14 +0530

    script

--- 06-variables.sh
+++ 06-variables.sh
@@ -2,11 +2,13 @@
 
 Starttime=$(date +%s)
 
-echo "time starts in $Starttime"
+echo "time starts at $Starttime"
 
 sleep 10
 
 Endtime=$(date +%s)
+
+echo "time ends at $Endtime"
 
 Totaltime=$(($Endtime-$Starttime))
 
